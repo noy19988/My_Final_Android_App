26df0a8cbe8bf828c157c254883f704c5e5dfdc4
Noy Amsalem <noyamsalem@Users-MBP.lan>
2024-12-24 19:03:28 +0200

    second commit

--- app/src/main/java/com/example/my_final_app/AddStudentActivity.kt
+++ app/src/main/java/com/example/my_final_app/AddStudentActivity.kt
@@ -1,29 +1,20 @@
 package com.example.my_final_app
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class AddStudentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_student)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val addStudentButton:Button = findViewById(R.id.main_activity_add_student_button)
-        addStudentButton.setOnClickListener{
-            val intent = Intent(this,AddStudentActivity::class.java)
-            startActivity(intent)
-            }
-        }
     }
+}
